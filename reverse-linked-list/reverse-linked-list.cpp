07fd7aef999f09fd16568640e9e1170ffee900d4
RISHAB PURKAYASTHA <63293155+rishab1128@users.noreply.github.com>
2020-12-09 02:56:23 +0530

    working commit - Created using LeetHub

---
+++ reverse-linked-list/reverse-linked-list.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+// struct ListNode 
+// {
+//     int val;
+//     ListNode *next;
+//     ListNode() : val(0), next(nullptr) {}
+//     ListNode(int x) : val(x), next(nullptr) {}
+//     ListNode(int x, ListNode *next) : val(x), next(next) {}
+// };
+​
+class Solution 
+{
+public:
+    Solution()
+    {
+        std::ios::sync_with_stdio(false);
+    }
+    ListNode* reverseList(ListNode* head) 
+    {
+        ListNode*curr=head;
+        ListNode*prev=NULL;
+        ListNode*next=NULL;
+        while(curr!=NULL)
+        {
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+};
