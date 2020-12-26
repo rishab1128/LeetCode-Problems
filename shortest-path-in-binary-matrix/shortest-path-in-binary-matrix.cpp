23cf6565645aefbac6f55bb46ac09525f219bd49
RISHAB PURKAYASTHA <63293155+rishab1128@users.noreply.github.com>
2020-12-26 10:03:22 +0530

    Time: 112 ms (74.56%), Space: 19.8 MB (67.17%) - LeetHub

--- shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,23 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+​
+class Solution 
+{
+public:
+    Solution()
+    {
+        std::ios::sync_with_stdio(false);
+    }
+​
+    int bfs(vector<vector<int>>& grid, int n)
+    {
+        vector<pair<int,int>>directions={{1,1},{0,1},{1,0},{0,-1},{-1,0},{-1, -1},{1, -1},{-1, 1}};
+        queue<pair<int,int>>q;
+        q.push(make_pair(0,0));
+        grid[0][0]=1;
+        while(!q.empty())
+        {
+            int x=q.front().first;
+            int y=q.front().second;
+            q.pop();
+            if(x==n-1&&y==n-1) 
+                return grid[x][y];
+            for(auto dir: directions)
+            {
+                int dx=x+dir.first;
                 int dy=y+dir.second;
                 if(dx<n&&dx>=0&&dy<n&&dy>=0&&grid[dx][dy]==0)
                 {
-                    grid[dx][dy]=grid[x][y]+1;
-                    q.push(make_pair(dx,dy));
-                }
-                    
-            }
-        }
-        
-        return -1;
-    }
-    
-    int shortestPathBinaryMatrix(vector<vector<int>>& grid) 
-    {
-        int n=grid.size();
-        if(grid[0][0]||grid[n-1][n-1])
-            return -1;
-        int ans=bfs(grid,n);
-        return ans;
-    }
-};
-​
