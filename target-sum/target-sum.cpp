a7cd672b1097f00356ceec4f6cd028dcb933a6f7
RISHAB PURKAYASTHA <63293155+rishab1128@users.noreply.github.com>
2020-12-30 18:29:49 +0530

    Time: 296 ms (45.70%), Space: 18.2 MB (38.71%) - LeetHub

--- target-sum/target-sum.cpp
+++ target-sum/target-sum.cpp
@@ -7,6 +7,7 @@ public:
     {
         std::ios::sync_with_stdio(false);
     }
+    int dp[1000][2001];
     int func(int*arr,int S,int sum,int n)
     {
         if(n==0&&sum==S)
@@ -15,13 +16,20 @@ public:
         }
         if(n<=0&&sum!=S)
             return 0;
+        if(dp[n][sum+1000]!=INT_MIN)
+            return dp[n][sum+1000];
         int left=func(arr+1,S,sum+arr[0],n-1);
         int right=func(arr+1,S,sum-arr[0],n-1);
-        return left+right;
+        return dp[n][sum+1000]=left+right;
     }
 ​
     int findTargetSumWays(vector<int>& nums, int S) 
     {
+        for(int i=0; i<1000; i++)
+        {
+            for(int j=0; j<2001; j++)
+                dp[i][j]=INT_MIN;
+        }
         int n=nums.size();
         int arr[n];
         int ct=0;
