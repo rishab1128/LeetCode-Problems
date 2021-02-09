f7047b4afff9121c0cd9708bfa063fedea11fc90
RISHAB PURKAYASTHA <63293155+rishab1128@users.noreply.github.com>
2021-02-09 22:02:36 +0530

    Time: 12 ms (81.47%), Space: 10.5 MB (79.87%) - LeetHub

---
+++ longest-common-subsequence/longest-common-subsequence.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string A , string B) 
+    {
+        int n=A.size(),m=B.size();
+        int dp[n+1][m+1];
+        int ans=0;
+        
+        //Note that dp[i][j] contains length of longest common subseq of X[0..i-1] and Y[0..j-1] 
+ 
+        for(int i=0; i<=n; i++)
+        {
+            for(int j=0; j<=m; j++)
+            {
+                if(i==0||j==0)
+                    dp[i][j]=0;
+                else if(A[i-1]==B[j-1])
+                {
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }
+                else
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                    
+            }
+        }
+        
+        return ans=dp[n][m];
+        
+    }
+};
