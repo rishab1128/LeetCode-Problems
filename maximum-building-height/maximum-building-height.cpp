716cfedd39ef17ecf4062da2618bb29f2bc36b2c
RISHAB PURKAYASTHA <63293155+rishab1128@users.noreply.github.com>
2021-04-30 23:01:48 +0530

    Time: 484 ms (52.24%), Space: 102.5 MB (38.48%) - LeetHub

---
+++ maximum-building-height/maximum-building-height.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int traverse(vector<vector<int>>&arr)
+    {
+        int m=arr.size() , ans=0;
+        for(int i=0; i<m-1; i++)
+        {
+            int h1=arr[i][1] , h2=arr[i+1][1];
+            int d1=arr[i][0] , d2=arr[i+1][0];
+            int h=h1+abs(d2-d1);
+            if(h>h2)
+                h=h2+(h-h2)/2;
+            ans=max(ans,h);
+            arr[i+1][1]=min(arr[i+1][1] , h);
+        }
+        return ans;
+    }
+    int maxBuilding(int n, vector<vector<int>>& res) 
+    {
+        res.insert(res.end(),{{1,0} ,{n,n-1}});
+        sort(res.begin(),res.end());
+        traverse(res);
+        reverse(res.begin(),res.end());
+        return traverse(res);
+    }
+};
