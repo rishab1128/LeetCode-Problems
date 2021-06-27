1eb2528c04bc7efab562f74647e54a6e53f2a796
Rishab Purkayastha <63293155+rishab1128@users.noreply.github.com>
2021-06-27 14:35:08 +0530

    Time: 520 ms (44.95%), Space: 209.3 MB (43.04%) - LeetHub

--- count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.cpp
+++ count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.cpp
@@ -1,31 +1,79 @@
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-#include <bits/stdc++.h>
- 
-using namespace __gnu_pbds;
-using namespace std;
- 
-typedef tree<
-    int,
-    null_type,
-    less_equal<int>,
-    rb_tree_tag,
-    tree_order_statistics_node_update> ordered_multiset;
-
 class Solution {
 public:
-    vector<int> countSmaller(vector<int>& nums)
+    
+    void merge(vector<pair<int,int>>&a, vector<int>&count , int start, int mid, int end)
     {
-        int n=nums.size();
-        vector<int>ans(n,0);
-        ordered_multiset st;
-        st.insert(nums[n-1]);
-        for(int i=n-2; i>=0; i--)
+        int i=start,j=mid,k=0;
+        vector<pair<int, int>>temp(end-start+1);
+        int ct=0;
+
+        while(i<mid&&j<=end)
         {
-            ans[i]=st.order_of_key(nums[i]);
-            st.insert(nums[i]);
+            if(a[i].first<=a[j].first)
+            {
+                temp[k]=a[i];
+                count[a[i].second]+=ct;
+                k++;
+                i++;
+            }
+            else
+            {
+                ct++;
+                temp[k]=a[j];
+                k++;
+                j++;
+            }
         }
-        return ans;
+
+        while(i<mid)
+        {
+            temp[k]=a[i];
+            count[a[i].second]+=ct;
+            k++;
+            i++;
+        }
+
+        while(j<=end)
+        {
+            temp[k]=a[j];
+            k++;
+            j++;
+        }
+
+        for(int i=start,k=0; i<=end; i++,k++)
+        {
+            a[i]=temp[k];
+        }
+
+    }
+
+    void merge_sort(vector<pair<int,int>>&a, vector<int>&count , int start, int end)
+    {
+        
+        if(start<end)
+        {
+
+           int mid=(start+end)/2;
+           merge_sort(a,count,start,mid);
+           merge_sort(a,count,mid+1,end);
+           merge(a,count,start,mid+1,end);
+        }
+
+    }
+    
+    vector<int> countSmaller(vector<int>& a)
+    {
+        int n=a.size();
+        vector<pair<int,int>>arr(n);
+        for(int i=0; i<n; i++)
+        {
+          arr[i]={a[i],i};
+        }
+
+        vector<int>count(n,0);
+        merge_sort(arr,count,0,n-1);
+
+        return count;
         
     }
 };
