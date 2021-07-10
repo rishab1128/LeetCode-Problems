d7d0c6910edb7cc259e9a6a6dee84b1278cb3444
Rishab Purkayastha <63293155+rishab1128@users.noreply.github.com>
2021-07-10 23:04:04 +0530

    Time: 120 ms (80.00%), Space: 31.4 MB (20.00%) - LeetHub

--- nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
+++ nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
@@ -11,24 +11,25 @@ public:
     int nearestExit(vector<vector<char>>& maze, vector<int>& ent) 
     {
         n=maze.size() , m=maze[0].size();
-        queue<pair<int,int>>q;
-        q.push({ent[0],ent[1]});
+        queue<pair<pair<int,int>, int>>q;
+        q.push({{ent[0],ent[1]},0});
         
         
-        maze[ent[0]][ent[1]]='+';
+        vector<vector<bool>>vis(n,vector<bool>(m,0));
+        vis[ent[0]][ent[1]]=1;
         
         
         vector<vector<int>> dir={{0,1},{1,0},{0,-1},{-1,0}};
-        int ct=1;
+        
         
         while(q.size()>0)
         {
             int t=q.size() ;
-            
             for(int z=0; z<t; z++)
             {
                 
-                int i=q.front().first , j=q.front().second;
+                int i=q.front().first.first , j=q.front().first.second;
+                int ct=q.front().second;
                 q.pop();
 
 
@@ -37,19 +38,17 @@ public:
                     int x=i+dir[l][0];
                     int y=j+dir[l][1];
 					
-                    if(!isValid(x,y) || maze[x][y]=='+')
+                    if(!isValid(x,y) || maze[x][y]=='+' || vis[x][y])
                         continue;
 					
+                    //cout<<x<<" "<<y<<" "<<ct<<endl;
                     if(x==0 || y==0 || x==n-1 || y==m-1)
-                        return ct;
+                        return ct+1;
 					
-                    maze[x][y]='+';
-                    q.push({x,y});
+                    vis[x][y]=1;
+                    q.push({{x,y},ct+1});
                 }
-                
             }
-            
-            ct++;
             
         }
         
