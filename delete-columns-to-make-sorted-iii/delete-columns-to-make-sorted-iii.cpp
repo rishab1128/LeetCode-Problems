09970fb126e122d230f404012f42300a498b902c
Rishab Purkayastha <63293155+rishab1128@users.noreply.github.com>
2021-07-16 06:04:04 +0530

    Time: 16 ms (87.61%), Space: 10.4 MB (53.21%) - LeetHub

---
+++ delete-columns-to-make-sorted-iii/delete-columns-to-make-sorted-iii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    //Idea similar to LIS 
+    int minDeletionSize(vector<string>& strs) 
+    {
+        int m=strs[0].size()  , n=strs.size();
+        vector<int>dp(m,1);
+        for(int j=1; j<m; j++)
+        {
+            for(int k=j-1; k>=0; k--)
+            {
+                int flag=1;
+                for(int i=0; i<n; i++)
+                {
+                    if(strs[i][j]>=strs[i][k])
+                        continue;
+                    else
+                    {    
+                        flag=0;
+                        break;
+                    }
+                }
+                if(flag)
+                    dp[j]=max({dp[j],dp[k]+1,1});
+            }
+        }
+        
+        // for(auto x: dp)
+        //     cout<<x<<" ";
+        // cout<<endl;
+        
+        int lis=*max_element(dp.begin(),dp.end());
+        return m-lis;
+    }
+};
