0de755cd45f528e0f5fbcf631bac87355e7e6111
Rishab Purkayastha <63293155+rishab1128@users.noreply.github.com>
2021-07-25 22:29:48 +0530

    Time: 4 ms (97.22%), Space: 8.3 MB (41.06%) - LeetHub

--- reverse-linked-list/reverse-linked-list.cpp
+++ reverse-linked-list/reverse-linked-list.cpp
@@ -1,34 +1,26 @@
-#include<bits/stdc++.h>
-using namespace std;
- 
-// struct ListNode 
-// {
-//     int val;
-//     ListNode *next;
-//     ListNode() : val(0), next(nullptr) {}
-//     ListNode(int x) : val(x), next(nullptr) {}
-//     ListNode(int x, ListNode *next) : val(x), next(next) {}
-// };
-​
-class Solution 
-{
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
 public:
-    Solution()
-    {
-        std::ios::sync_with_stdio(false);
-    }
-    ListNode* reverseList(ListNode* head) 
-    {
-        ListNode*curr=head;
-        ListNode*prev=NULL;
-        ListNode*next=NULL;
-        while(curr!=NULL)
-        {
-            next=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=next;
-        }
-        return prev;
-    }
+    ListNode* reverseList(ListNode* head) 
+    {
+        ListNode*dummy=NULL,*next=head;
+        
+        while(head!=NULL)
+        {
+            next=head->next;
+            head->next=dummy;
+            dummy=head;
+            head=next;
+        }
+        return dummy;
+    }
 };
