63a0492c30a681782f348bd18afed5403b59ecdd
Rishab Purkayastha <63293155+rishab1128@users.noreply.github.com>
2021-07-30 03:22:35 +0530

    Time: 228 ms (34.14%), Space: 117.9 MB (53.84%) - LeetHub

--- palindrome-linked-list/palindrome-linked-list.cpp
+++ palindrome-linked-list/palindrome-linked-list.cpp
@@ -1,16 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-        return check(head, head);
+    ListNode*findMiddle(ListNode*head)
+    {
+        ListNode*slow=head , *fast=head;
+        while(fast->next!=NULL and fast->next->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
     }
     
-    bool check(ListNode*& head, ListNode* p) {
-        if(!p) { return true; }
-        bool isPal = check(head, p->next);
-        if(head->val != p->val) {
-            return false;
+    ListNode*reverse(ListNode*head)
+    {
+        ListNode*dummy=NULL,*next=head;
+        while(head!=NULL)
+        {
+            next=head->next;
+            head->next=dummy;
+            dummy=head;
+            head=next;
         }
-        head = head->next;
-        return isPal;
+        return dummy;
+    }
+    
+    bool isPalindrome(ListNode* head) 
+    {
+        //Find the middle element of the LL
+        ListNode*mid=findMiddle(head);
+        
+        
+        //Reverse the right-half of the LL(right half after the mid node)
+        mid->next=reverse(mid->next);
+        
+        
+        //Put a dummy node at head and Increment mid by one and then compare both of them simultaneoulsy
+        ListNode*dummy=head;
+        mid=mid->next;
+        while(mid!=NULL)
+        {
+            if(mid->val!=dummy->val)
+                return false;
+            mid=mid->next;
+            dummy=dummy->next;
+        }
+        return true;
     }
 };
