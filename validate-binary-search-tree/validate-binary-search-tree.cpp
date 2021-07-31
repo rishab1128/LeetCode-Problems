c41e840f1441dc52218cb244a406de70f45ac5ad
Rishab Purkayastha <63293155+rishab1128@users.noreply.github.com>
2021-07-31 06:26:56 +0530

    Time: 4 ms (98.73%), Space: 21.8 MB (15.77%) - LeetHub

--- validate-binary-search-tree/validate-binary-search-tree.cpp
+++ validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -1,46 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
-​
+
 // struct TreeNode 
 // {
-//     int val;
-//     TreeNode *left;
-//     TreeNode *right;     
-//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-//     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {} 
+//     int val;
+//     TreeNode *left;
+//     TreeNode *right;     
+//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {} 
 // };
-​
+
 class Solution 
 {
 public:
-    Solution()
-    {
-        std::ios::sync_with_stdio(false);
-    }
-​
-    void inorder(TreeNode*root,vector<int>&arr)
-    {
-        if(!root)
-            return;
-        inorder(root->left,arr);
-        arr.push_back(root->val);
-        inorder(root->right,arr);
-    }
-​
-    bool isValidBST(TreeNode* root) 
-    {
-        vector<int>arr;
-        inorder(root,arr);
-        if(is_sorted(arr.begin(),arr.end()))
-        {
-            for(int i=1; i<(int)arr.size(); i++)
-            {
-                if(arr[i]==arr[i-1])
-                    return false;
-            }
-            return true;
-        }
-        return false;
-    }
+    Solution()
+    {
+        std::ios::sync_with_stdio(false);
+    }
+
+    bool isValidBST2(TreeNode* root , TreeNode*&prev) 
+    {
+        //Using inorder traversal
+        if(root)
+        {
+            if(!isValidBST2(root->left,prev))
+                return false;
+                
+            if(prev and prev->val >= root->val)
+                return false;
+                
+            prev=root;
+            
+            return isValidBST2(root->right,prev);
+        }
+        return true;
+    }
+    bool isValidBST(TreeNode*root)
+    {
+        TreeNode*prev=NULL;
+        return isValidBST2(root,prev);
+    }
 };
