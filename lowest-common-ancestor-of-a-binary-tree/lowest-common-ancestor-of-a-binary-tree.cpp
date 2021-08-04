db47625a889c4c0e719c87faab199366c0fac1c0
Rishab Purkayastha <63293155+rishab1128@users.noreply.github.com>
2021-08-04 14:00:11 +0530

    Time: 12 ms (94.42%), Space: 14.3 MB (57.41%) - LeetHub

--- lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,50 +9,20 @@
  */
 class Solution {
 public:
-    bool func(TreeNode*root,TreeNode*a, vector<TreeNode*>&li)
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) 
     {
-        if(root->val==a->val)
-        {
-            return true;
-        }
-        li.push_back(root);
-        if(root->left)
-            if(func(root->left,a,li))
-                return true;
-        if(root->right)
-            if(func(root->right,a,li))
-                return true;
-        li.pop_back();
-        return false;
-    }
-
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* a, TreeNode* b) 
-    {
-        TreeNode*ans=root;
-        vector<TreeNode*>li1,li2;
-        func(root,a,li1);
-        func(root,b,li2);
-        li1.push_back(a);
-        li2.push_back(b);
-        cout<<li1.size()<<endl;
-        cout<<li2.size()<<endl;
-        for(auto x: li1)
-            cout<<x->val<<" ";
-        cout<<endl;
-        for(auto x: li2)
-            cout<<x->val<<" ";
-        if(li1.size()>li2.size())
-            swap(li1,li2);
-        int n=li1.size() ;
-        for(int i=n-1; i>=0; i--)
-        {
-            if(li1[i]==li2[i])
-            {
-                ans=li1[i];
-                break;
-            }
-        }
-        return ans;
-
+        if(!root)
+            return NULL;
+        
+        if(root->val == p->val || root->val==q->val)
+            return root;
+        
+        TreeNode*leftLCA=lowestCommonAncestor(root->left , p,q);
+        TreeNode*rightLCA=lowestCommonAncestor(root->right , p,q);
+        
+        if(leftLCA and rightLCA)
+            return root;
+        
+        return  leftLCA==NULL?rightLCA:leftLCA;
     }
 };
