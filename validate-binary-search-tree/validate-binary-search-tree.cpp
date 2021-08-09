42b12982ce1015db9ef3925106b5c39126c4e83a
Rishab Purkayastha <63293155+rishab1128@users.noreply.github.com>
2021-08-10 01:24:13 +0530

    Time: 8 ms (87.96%), Space: 21.7 MB (31.52%) - LeetHub

--- validate-binary-search-tree/validate-binary-search-tree.cpp
+++ validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -1,44 +1,36 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-// struct TreeNode 
-// {
-//     int val;
-//     TreeNode *left;
-//     TreeNode *right;     
-//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-//     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {} 
-// };
-
-class Solution 
-{
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
 public:
-    Solution()
+    //Using Iterative Inorder Traversal Techq
+    bool isValidBST(TreeNode* root) 
     {
-        std::ios::sync_with_stdio(false);
-    }
-
-    bool isValidBST2(TreeNode* root , TreeNode*&prev) 
-    {
-        //Using inorder traversal
-        if(root)
+        stack<TreeNode*>st;
+        TreeNode*prev=NULL;
+        while(!st.empty() || root!=NULL)
         {
-            if(!isValidBST2(root->left,prev))
+            while(root!=NULL)
+            {
+                st.push(root);
+                root=root->left;
+            }
+            root=st.top();
+            st.pop();
+            if(prev and prev->val>=root->val)
                 return false;
-                
-            if(prev and prev->val >= root->val)
-                return false;
-                
             prev=root;
-            
-            return isValidBST2(root->right,prev);
+            root=root->right;
         }
         return true;
-    }
-    bool isValidBST(TreeNode*root)
-    {
-        TreeNode*prev=NULL;
-        return isValidBST2(root,prev);
+        
     }
 };
