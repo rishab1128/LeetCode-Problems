6ccb040f9dfa2bee2ce7c7e663a404673d6f45c0
Rishab Purkayastha <63293155+rishab1128@users.noreply.github.com>
2021-08-20 18:49:15 +0530

    Time: 60 ms (65.08%), Space: 19.2 MB (84.67%) - LeetHub

--- shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,30 +1,41 @@
-#include<bits/stdc++.h>
-using namespace std;
-​
-class Solution 
-{
+class Solution {
 public:
-    Solution()
-    {
-        std::ios::sync_with_stdio(false);
-    }
-​
-    int bfs(vector<vector<int>>& grid, int n)
-    {
-        vector<pair<int,int>>directions={{1,1},{0,1},{1,0},{0,-1},{-1,0},{-1, -1},{1, -1},{-1, 1}};
-        queue<pair<int,int>>q;
-        q.push(make_pair(0,0));
-        grid[0][0]=1;
-        while(!q.empty())
-        {
-            int x=q.front().first;
-            int y=q.front().second;
-            q.pop();
-            if(x==n-1&&y==n-1) 
-                return grid[x][y];
-            for(auto dir: directions)
-            {
-                int dx=x+dir.first;
-                int dy=y+dir.second;
-                if(dx<n&&dx>=0&&dy<n&&dy>=0&&grid[dx][dy]==0)
-                {
+    int shortestPathBinaryMatrix(vector<vector<int>>& arr) 
+    {
+        int n=arr.size();
+        if(arr[0][0] || arr[n-1][n-1])
+            return -1;
+        
+        queue<pair<int,int>>q;
+        vector<vector<int>>dir={{1,0},{-1,0},{0,1},{0,-1},{-1,-1},{-1,1},{1,-1},{1,1}};
+        
+        q.push({0,0});
+        arr[0][0]=1;
+        int ct=1;
+        
+        while(q.size()>0)
+        {
+            int sz=q.size();
+            while(sz--)
+            {
+                int x=q.front().first , y=q.front().second;
+                q.pop();
+                if(x==n-1 and y==n-1)
+                    return ct;
+                
+                for(int i=0; i<8; i++)
+                {
+                    int dx=x+dir[i][0] , dy=y+dir[i][1];
+                    if(dx>=0 and dx<n and dy>=0 and dy<n and arr[dx][dy]==0)
+                    {
+                        q.push({dx,dy});
+                        arr[dx][dy]=1;
+                    }
+                }
+            }
+            if(q.size()>0)
+                ct++;
+        }
+        return -1;
+    }
+};
