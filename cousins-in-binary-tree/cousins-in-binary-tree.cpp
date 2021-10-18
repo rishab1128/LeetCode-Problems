2e36a530722dde857dc0bb1a2824912e624d88c3
Rishab Purkayastha <63293155+rishab1128@users.noreply.github.com>
2021-10-18 10:58:56 +0530

    Time: 3 ms (68.56%), Space: 11 MB (46.41%) - LeetHub

--- cousins-in-binary-tree/cousins-in-binary-tree.cpp
+++ cousins-in-binary-tree/cousins-in-binary-tree.cpp
@@ -1,45 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
 // struct TreeNode 
 // {
-//     int val;
-//     TreeNode *left;
-//     TreeNode *right;     
-//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-//     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {} 
+//     int val;
+//     TreeNode *left;
+//     TreeNode *right;     
+//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {} 
 // };
-​
+
 class Solution 
 {
 public:
-    Solution()
-    {
-        std::ios::sync_with_stdio(false);
-    }
-    int depth[101],par[101];
-    void dfs(TreeNode*root,TreeNode*parent)
-    {
-        if(root)
-        {
-            depth[root->val]=1+depth[parent->val];
-            par[root->val]=parent->val;
-            dfs(root->left,root);
-            dfs(root->right,root);
-        }
-    }
-    bool isCousins(TreeNode* root, int x, int y) 
-    {
-        memset(depth,0,sizeof(depth));
-        memset(par,-1,sizeof(par));
-        depth[root->val]=-1;
-        dfs(root,root);
-        /*for(int i=1; i<=100; i++)
-        {
-            cout<<i<<" "<<depth[i]<<" "<<par[i]<<endl;
-        }*/
-        if(depth[x]==depth[y]&&par[x]!=par[y]&&par[x]!=-1&&par[y]!=-1)
-            return true;
-        
-        return false;
-    }
+    Solution()
+    {
+        std::ios::sync_with_stdio(false);
+    }
+    int depth[101],par[101];
+    void dfs(TreeNode*root,TreeNode*parent)
+    {
+        if(root)
+        {
+            depth[root->val]=1+depth[parent->val];
+            par[root->val]=parent->val;
+            dfs(root->left,root);
+            dfs(root->right,root);
+        }
+    }
+    bool isCousins(TreeNode* root, int x, int y) 
+    {
+        memset(depth,0,sizeof(depth));
+        memset(par,-1,sizeof(par));
+        depth[root->val]=-1;
+        dfs(root,root);
+        /*for(int i=1; i<=100; i++)
+        {
+            cout<<i<<" "<<depth[i]<<" "<<par[i]<<endl;
+        }*/
+        if(depth[x]==depth[y]&&par[x]!=par[y]&&par[x]!=-1&&par[y]!=-1)
+            return true;
+        
+        return false;
+    }
 };
-​
