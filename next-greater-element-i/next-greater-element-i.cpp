10d943b44740d924499c55b835c6f2f18f59278f
Rishab Purkayastha <63293155+rishab1128@users.noreply.github.com>
2021-10-19 12:53:08 +0530

    Time: 8 ms (59.54%), Space: 9.2 MB (6.18%) - LeetHub

--- next-greater-element-i/next-greater-element-i.cpp
+++ next-greater-element-i/next-greater-element-i.cpp
@@ -1,29 +1,48 @@
 #include<bits/stdc++.h>
 using namespace std;
-​
+
 class Solution 
 {
 public:
-    Solution()
-    {
-        std::ios::sync_with_stdio(false);
-    }
-​
-    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) 
-    {
-        if(nums1.size()==0)
-            return nums1;
-        else if(nums2.size()==0)
-            return nums2;
-        
-        stack<int>st;
-        int n=nums2.size();
-        st.push(nums2[0]);
-        map<int,int>mp;
-        for(int i=1; i<n; i++)
-        {
-            int next=nums2[i];
-            while(next>st.top()&&!st.empty())
-            {
-                mp[st.top()]=next;
-                st.pop();
+    Solution()
+    {
+        std::ios::sync_with_stdio(false);
+    }
+
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) 
+    {
+        if(nums1.size()==0)
+            return nums1;
+        else if(nums2.size()==0)
+            return nums2;
+        
+        stack<int>st;
+        int n=nums2.size();
+        st.push(nums2[0]);
+        map<int,int>mp;
+        for(int i=1; i<n; i++)
+        {
+            int next=nums2[i];
+            while(next>st.top()&&!st.empty())
+            {
+                mp[st.top()]=next;
+                st.pop();
+                if(st.empty())
+                    break;
+            }
+            st.push(next);
+        }
+        while(!st.empty())
+        {
+            int x=st.top();
+            mp[x]=-1;
+            st.pop();
+        }
+        vector<int>res;
+        for(int i=0; i<(int)nums1.size(); i++)
+        {
+            res.push_back(mp[nums1[i]]);
+        }
+        return res;
+    }
+};
