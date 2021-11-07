2a6e6432178168874718b586954a241e8382d89a
Rishab Purkayastha <63293155+rishab1128@users.noreply.github.com>
2021-11-07 23:37:42 +0530

    Time: 176 ms (99.73%), Space: 131.9 MB (74.99%) - LeetHub

--- sliding-window-maximum/sliding-window-maximum.cpp
+++ sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,33 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
-​
+
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& arr, int k) 
-    {
-        deque<int>dq;
-        vector<int>ans;
-        int n=arr.size();
-        for(int i=0; i<k-1; i++)
-        {
-            while(!dq.empty()&&arr[dq.back()]<=arr[i])
-                dq.pop_back();
-            dq.push_back(i);
-        }
-        for(int i=k-1; i<n; i++)
-        {
-            while(!dq.empty()&&dq.front()<=i-k)
-                dq.pop_front();
-            
-            while(!dq.empty()&&arr[dq.back()]<=arr[i])
-                dq.pop_back();
-            
-            dq.push_back(i);
-            ans.push_back(arr[dq.front()]);
-        }
-        // while(!dq.empty())
-        //     ans.push_back(arr[dq.front()]),dq.pop_front();
-        return ans;
-        
-    }
+    Solution()
+    {
+        std::ios::sync_with_stdio(false);
+    }
+    vector<int> maxSlidingWindow(vector<int>& arr, int k) 
+    {
+        //Decreasing DQ
+        deque<int>dq;
+        vector<int>ans;
+        int n=arr.size();
+        for(int i=0; i<k-1; i++)
+        {
+            while(!dq.empty()&&arr[dq.back()]<=arr[i])
+                dq.pop_back();
+            dq.push_back(i);
+        }
+        for(int i=k-1; i<n; i++)
+        {
+            while(!dq.empty()&&dq.front()<=i-k)
+                dq.pop_front();
+            
+            while(!dq.empty()&&arr[dq.back()]<=arr[i])
+                dq.pop_back();
+            
+            dq.push_back(i);
+            ans.push_back(arr[dq.front()]);
+        }
+        
+        return ans;
+        
+    }
 };
