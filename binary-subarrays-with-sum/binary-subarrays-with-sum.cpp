db2b58e951e65e64b1b1098d07ed78ee2f23bd3e
Rishab Purkayastha <63293155+rishab1128@users.noreply.github.com>
2021-11-20 19:48:37 +0530

    Time: 48 ms (64.31%), Space: 28.8 MB (75.91%) - LeetHub

--- binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    int numSubarraysWithSum(vector<int>& arr, int goal) 
+    //This ques is same as  : Count number of subarrays with exactly k number of ones in it
+    int numSubarraysWithSum(vector<int>& arr, int k) 
     {
-        int n=arr.size();
-        vector<int>pref(n);
-        pref[0]=arr[0];
-        for(int i=1; i<n; i++)
-            pref[i]=pref[i-1]+arr[i];
+        //Let , number of subarrays with atmost k no. of ones = count(arr,k);
+        //Count number of subarrays with exactly k number of ones in it = count(arr,k) - count(arr,k-1);
         
-        unordered_map<int,int>mp;
-        mp[0]++;
-        int ans=0;
-        for(int i=0; i<n; i++)
-        {
-            int target=pref[i]-goal;
-            ans+=mp[target];
-            mp[pref[i]]++;
-        }
-        
+        int ans=k>0 ? count(arr,k)-count(arr,k-1) : count(arr,k);
         return ans;
-        
+    }
+    int count(vector<int>&a , int k)
+    {
+        int i=0,cursum=0 , res=0 , n=a.size();
+        for(int j=0;j<n;j++)
+        {
+           cursum+=a[j];
+           while(i<n && cursum>k)
+           {
+               cursum-=a[i++];
+           }
+           res+=(j-i+1);
+        }
+        return res;
     }
 };
