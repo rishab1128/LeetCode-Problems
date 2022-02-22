9481e6991dfbe67f75bc3579b3d24d20144600fd
Rishab Purkayastha <63293155+rishab1128@users.noreply.github.com>
2022-02-22 14:05:58 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (28.55%) - LeetHub

---
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int titleToNumber(string s) 
+    {
+        reverse(s.begin(),s.end());
+        int n=s.size(),ans=0;
+        for(int i=0; i<n; i++)
+        {
+            ans+=(pow(26,i)*(s[i]-65+1));
+        }
+        return ans;
+        
+    }
+};
