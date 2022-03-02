ddd6eda9f41b4e7d38477275a5d35f369ab3d5bc
Rishab Purkayastha <63293155+rishab1128@users.noreply.github.com>
2022-03-02 22:39:51 +0530

    Time: 4 ms (36.85%), Space: 6.8 MB (22.15%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        unordered_map<char, vector<int>> hash; //chars -> list of indices;
+        for(int i=0; i<t.length();i++) hash[t[i]].push_back(i);
+        
+        int prev = -1;
+        for(auto c : s){
+            auto it = hash.find(c);
+            if(it == hash.end()) return false;
+            auto vec = it->second;
+            int pos = upper_bound(vec.begin(), vec.end(), prev) - vec.begin(); // find the first pos whose value is greater than prev
+            if(pos == vec.size()) return false;
+            prev = vec[pos];
+        }
+        return true;
+    }
+};
