e2940aa5abca5580729d051b11fc2b0516d00969
Rishab Purkayastha <63293155+rishab1128@users.noreply.github.com>
2022-04-16 22:35:34 +0530

    Time: 49 ms (61.70%), Space: 35.9 MB (5.52%) - LeetHub

---
+++ 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    Solution()
+    {
+        std::ios::sync_with_stdio(false);
+    }
+    void inorder(TreeNode*root, vector<int>&a)
+    {
+        if(root)
+        {
+            inorder(root->left,a);
+            a.push_back(root->val);
+            inorder(root->right,a);
+        }
+    }
+    
+    void func(TreeNode**root,map<int,int>&mp)
+    {
+        if(*root)
+        {
+            (*root)->val=mp[(*root)->val];
+            func(&((*root)->left),mp);
+            func(&((*root)->right),mp);
+        }
+    }
+    
+    TreeNode* convertBST(TreeNode* root) 
+    {
+        vector<int>a;
+        inorder(root,a);
+        int n=a.size();
+        if(n==0)
+            return root;
+        map<int,int>mp;
+        int suff[n];
+        suff[n-1]=a[n-1];
+        mp[a[n-1]]=suff[n-1];
+        for(int i=n-2; i>=0; i--)
+        {
+            suff[i]=suff[i+1]+a[i];
+            mp[a[i]]=suff[i];
+        }
+        func(&root,mp);
+        return root;
+        
+    }
+};
