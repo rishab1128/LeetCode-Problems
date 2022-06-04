88593fb165e3a7e70fa7640988aa2de3d4661db5
Rishab Purkayastha <63293155+rishab1128@users.noreply.github.com>
2022-06-04 15:13:23 +0530

    Time: 756 ms (12.52%), Space: 69.8 MB (71.94%) - LeetHub

--- 2271-maximum-white-tiles-covered-by-a-carpet/2271-maximum-white-tiles-covered-by-a-carpet.cpp
+++ 2271-maximum-white-tiles-covered-by-a-carpet/2271-maximum-white-tiles-covered-by-a-carpet.cpp
@@ -3,37 +3,24 @@ public:
     int maximumWhiteTiles(vector<vector<int>>& tiles, int len) 
     {
         int n = tiles.size();
-        sort(tiles.begin() , tiles.end());
-        vector<int>pref(n+1) , endIdx(n);
-        pref[0]=0;
-        for(int i=1; i<=n; i++){
-            pref[i]=pref[i-1]+(tiles[i-1][1]-tiles[i-1][0]+1);
-            endIdx[i-1]=tiles[i-1][1];
-        }
-        
-        // for(auto x: pref)
-        //     cout<<x<<" ";
-        // cout<<endl;
-        // for(auto x: endIdx)
-        //     cout<<x<<" ";
-        // cout<<endl;
-        
-        int ans=0;
-        for(int i=0; i<n; i++)
+        int start=0,end=0,full=0,partial=0,ans=0;
+        sort(tiles.begin(),tiles.end());
+        while(end<n and ans<len)
         {
-            int start = tiles[i][0];
-            int end = start+len-1;
-            
-            int idx = upper_bound(endIdx.begin(),endIdx.end(),end)-endIdx.begin();
-            int sum = pref[idx]-pref[i];
-            if(idx<n and end>=tiles[idx][0])
-                sum+=end-tiles[idx][0]+1;
-            
-            // cout<<start<<" "<<end<<" "<<sum<<endl;
-            
-            ans=max(ans,sum);
+            if(tiles[start][0]+len>tiles[end][1])
+            {
+                full+=tiles[end][1]-tiles[end][0]+1;
+                ans=max(ans,full);
+                end++;
+            }
+            else
+            {
+                partial=max(0,tiles[start][0]+len-tiles[end][0]);
+                ans=max(ans,full+partial);
+                full-=(tiles[start][1]-tiles[start][0]+1);
+                start++;
+            }
         }
-        
         return ans;
         
     }
