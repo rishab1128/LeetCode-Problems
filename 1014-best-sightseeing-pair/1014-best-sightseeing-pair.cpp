c191b4e2dfa5c22269856469217944bca16c9c1e
Rishab Purkayastha <63293155+rishab1128@users.noreply.github.com>
2022-06-28 01:50:35 +0530

    Time: 84 ms (44.36%), Space: 41 MB (15.17%) - LeetHub

---
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxScoreSightseeingPair(vector<int>& arr) 
+    {
+        //dp[0] = 0;
+        //dp[i] = dp[i-1]-arr[i-1]+arr[i]-1 ;
+        //Ans  = *max_element of dp array
+        //dp[j] := maxSum/(finds appropriate i) if that element is chosen as my arr[j] 
+        
+        
+        int n = arr.size();
+        vector<int>dp(n,0);
+        dp[0]= 0 ;
+        for(int i=1; i<n; i++)
+        {
+            dp[i] = max( dp[i-1]-arr[i-1]+arr[i]-1 , arr[i]+arr[i-1]-1);
+        }
+        return *max_element(dp.begin(),dp.end());
+        
+    }
+};
