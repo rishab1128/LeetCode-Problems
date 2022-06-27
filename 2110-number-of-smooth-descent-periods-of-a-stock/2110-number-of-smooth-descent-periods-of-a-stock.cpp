82729b2a60ec465974d956625241e81ea4111f1f
Rishab Purkayastha <63293155+rishab1128@users.noreply.github.com>
2022-06-28 02:19:35 +0530

    Time: 268 ms (22.63%), Space: 101.4 MB (7.94%) - LeetHub

--- 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.cpp
+++ 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.cpp
@@ -4,15 +4,12 @@ public:
     {
         using ll =long long;
         int n = arr.size();
-        vector<ll>dp(n,0);
-        dp[0]=1;
+        vector<ll>dp(n,1);
         ll ans = 1;
         for(int i=1; i<n; i++)
         {
             if(arr[i-1]-arr[i]==1)
                 dp[i]=dp[i-1]+1;
-            else
-                dp[i]=1;
             ans += dp[i];
         }
         return ans;
