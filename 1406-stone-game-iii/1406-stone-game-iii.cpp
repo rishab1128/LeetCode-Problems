dc70014a479ad1ec1f4350984e987b2b455f2d1f
Rishab Purkayastha <63293155+rishab1128@users.noreply.github.com>
2022-06-27 05:43:53 +0530

    Time: 313 ms (70.66%), Space: 148.2 MB (15.77%) - LeetHub

---
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    
+    vector<int>dp;
+    int n;
+    const int INF = -0x3f3f3f3f;
+    
+    int recurse(vector<int> &arr, int idx)
+    {
+        if(idx>=n)
+            return 0;
+        
+        if(dp[idx]!=INF)
+            return dp[idx];
+        
+        
+        int op1 = INT_MIN , op2 = INT_MIN , op3 = INT_MIN;
+        
+        op1 = arr[idx] - recurse(arr,idx+1);
+        
+        if(idx+1<n)
+            op2 = arr[idx]+arr[idx+1]-recurse(arr,idx+2);
+        
+        if(idx+2<n)
+            op3 = arr[idx]+arr[idx+1]+arr[idx+2]-recurse(arr,idx+3);
+        
+        return dp[idx] = max({op1,op2,op3});
+    }
+
+    string stoneGameIII(vector<int>& arr) 
+    {
+        n  = arr.size();
+        dp.resize(n+1,INF);
+        int profit = recurse(arr,0);
+        // cout<<profit<<endl;
+        if(profit>0)
+            return "Alice";
+        else if(profit==0)
+            return "Tie";
+        return "Bob";
+    }
+};
