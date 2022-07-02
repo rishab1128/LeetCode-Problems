61fdc9e44c86d9a8bc14b2ecf19945f3b06d363e
Rishab Purkayastha <63293155+rishab1128@users.noreply.github.com>
2022-07-03 02:52:39 +0530

    Time: 773 ms (14.02%), Space: 200.5 MB (12.15%) - LeetHub

--- 1994-the-number-of-good-subsets/1994-the-number-of-good-subsets.cpp
+++ 1994-the-number-of-good-subsets/1994-the-number-of-good-subsets.cpp
@@ -3,19 +3,24 @@ const int mod = 1e9+7;
 class Solution {
 public:
     
+    //TC/SC : O(brr.size() * max Value of mask) == O(brr.size() * 1024) == O(18*1024) [ Since brr.size() can have at most 18 elements]
+    
     unordered_map<ll,ll>freq;
     
-    ll recur(vector<ll>&brr, ll idx, ll mask)
+    ll recur(vector<ll>&brr, ll idx, ll mask, vector<vector<ll>>&dp)
     {
         if(idx==brr.size())
             return (mask>0);
         
         if(brr[idx]&mask)
-            return recur(brr,idx+1,mask) % mod;
+            return recur(brr,idx+1,mask,dp) % mod;
         
-        ll op1 = (freq[brr[idx]] % mod * recur(brr,idx+1,mask|brr[idx]) % mod) % mod;
-        ll op2 = recur(brr,idx+1,mask) % mod;
-        return (op1 % mod + op2 % mod) % mod;
+        if(dp[idx][mask]!=-1)
+            return dp[idx][mask];
+        
+        ll op1 = (freq[brr[idx]] % mod * recur(brr,idx+1,mask|brr[idx],dp) % mod) % mod;
+        ll op2 = recur(brr,idx+1,mask,dp) % mod;
+        return dp[idx][mask] = (op1 % mod + op2 % mod) % mod;
     }
     
     int numberOfGoodSubsets(vector<int>& arr) 
@@ -28,6 +33,11 @@ public:
         ll ones = 0;
         for(int i=0; i<n; i++)
         {
+            if(arr[i]==1)
+            {
+                ones++;
+                continue;
+            }
             
             if(arr[i]%4==0 || arr[i]%9==0 || arr[i]%25==0)
                 continue;
@@ -35,30 +45,8 @@ public:
             ll num = 0 , flag=1;
             for(int j=0; j<primes.size(); j++)
             {
-                ll ct = 0;
-                while(arr[i]%primes[j]==0)
-                {
-                    arr[i]/=primes[j];
-                    ct++;
-                    if(ct>1)
-                        break;
-                }
-                if(ct>1)
-                {
-                    flag=0;
-                    break;
-                }
-                else if(ct==1)
+                if(arr[i]%primes[j]==0)
                     num+=(1<<j);
-            }
-            
-            if(!flag)
-                continue;
-            
-            if(num==0)
-            {
-                ones++;
-                continue;
             }
             
             freq[num]++;
@@ -66,6 +54,7 @@ public:
             if(freq[num]==1)
                 brr.push_back(num);
         }
+        
         
         /*cout<<brr.size()<<endl;
         vector<ll>tmp;
@@ -78,14 +67,18 @@ public:
                     oriNum*=primes[i];
             }
             tmp.push_back(oriNum);
+            cout<<num<<" ";
         }
+        cout<<endl;
         
         sort(tmp.begin(),tmp.end());
         for(auto x: tmp)
             cout<<x<<" ";
         cout<<endl;*/
         
-        ll ans = recur(brr,0,0) % mod;
+        vector<vector<ll>>dp(brr.size(),vector<ll>(1025,-1));
+        
+        ll ans = recur(brr,0,0,dp) % mod;
         
         while(ones--)
             ans = ( (ll)ans*2LL)%mod;
