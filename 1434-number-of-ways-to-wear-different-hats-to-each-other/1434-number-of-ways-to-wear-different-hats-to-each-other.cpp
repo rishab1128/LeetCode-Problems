4c795e036b4fe8fffe2da9483957151a947421f6
Rishab Purkayastha <63293155+rishab1128@users.noreply.github.com>
2022-07-03 04:43:10 +0530

    Time: 31 ms (78.87%), Space: 8.2 MB (73.24%) - LeetHub

--- 1434-number-of-ways-to-wear-different-hats-to-each-other/1434-number-of-ways-to-wear-different-hats-to-each-other.cpp
+++ 1434-number-of-ways-to-wear-different-hats-to-each-other/1434-number-of-ways-to-wear-different-hats-to-each-other.cpp
@@ -1,50 +1,49 @@
-using ll = long long;
 class Solution {
 public:
-    vector<vector<int>>hats;
-    const int mod = 1e9+7;
-    int n;
-    int dp[41][(1<<10)+1];
     
-    ll recur(ll idx, ll peopleMask)
+    vector<vector<int>>arr;
+    int n;
+    int dp[41][1025];
+    const int mod = 1e9+7;
+    
+    int recur(int i, int mask)
     {
-        if(peopleMask==pow(2,n)-1)
+        if(mask == (1<<n)-1)
             return 1;
         
-        if(idx>40)
+        if(i>40)
             return 0;
         
-        if(dp[idx][peopleMask]!=-1)
-            return dp[idx][peopleMask];
+        if(dp[i][mask]!=-1)
+            return dp[i][mask];
         
-        ll ans = recur(idx+1,peopleMask); //Don't wear this hat
-        for(auto person : hats[idx])
+        int op1 = recur(i+1,mask); //do not take the ith hat
+        int op2 = 0;
+        for(auto person : arr[i])
         {
-            ll num = (1<<person);
-            if(peopleMask&num) // person is already taken / person has already a hat with him
+            if(mask&(1<<person)) //person is already wearing a hat, so he cannot wear this ith hat
                 continue;
-            ans += recur(idx+1,peopleMask+num);
-            ans %= mod;
+            op2 = (op2 % mod + recur(i+1,mask+(1<<person)) % mod) % mod;
         }
-        return dp[idx][peopleMask] = ans;
+        return dp[i][mask] = (op1 % mod + op2 % mod) % mod;
     }
     
-    int numberWays(vector<vector<int>>& arr) 
+    int numberWays(vector<vector<int>>& hats) 
     {
-        n  = arr.size();
-        hats.resize(41);
+        arr.resize(41);
         memset(dp,-1,sizeof(dp));
         
-        //hats[i] -> stores all the people who can wear hat i
+        //arr[i] := stores all the people who can wear the ith hat
         
+        n = hats.size();
         for(int i=0; i<n; i++)
         {
-            for(auto hat : arr[i])
-                hats[hat].push_back(i);
+            for(auto hat : hats[i])
+                arr[hat].push_back(i);
         }
         
-         
-        ll ans = recur(1,0);
+        int ans = recur(0,0);
+        
         return ans;
         
     }
