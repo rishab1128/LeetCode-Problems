ea2ff8337895aa6c4a98451f1cc413d7d676abd7
Rishab Purkayastha <63293155+rishab1128@users.noreply.github.com>
2022-07-03 05:35:38 +0530

    Time: 7 ms (14.97%), Space: 6.2 MB (35.00%) - LeetHub

--- 678-valid-parenthesis-string/678-valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.cpp
@@ -1,32 +1,53 @@
 class Solution {
 public:
-    
-    int n;
-    int dp[101][101][101];
-    
-    int recur(string &s, int idx, int op, int cl)
-    {
-        if(op==cl and idx==n)
-            return 1;
-        if((cl>op) || idx>=n)
-            return 0;
-        
-        if(dp[idx][op][cl]!=-1)
-            return dp[idx][op][cl];
-        
-        if(s[idx]=='(')
-            return dp[idx][op][cl] = recur(s,idx+1,op+1,cl);
-        if(s[idx]==')')
-            return dp[idx][op][cl] = recur(s,idx+1,op,cl+1);
-        
-        return dp[idx][op][cl] = recur(s,idx+1,op+1,cl) || recur(s,idx+1,op,cl+1) || recur(s,idx+1,op,cl);
-    }
-    
     bool checkValidString(string s) 
     {
-        memset(dp,-1,sizeof(dp));
-        n = s.size();
-        int ans = recur(s,0,0,0);
-        return ans;
+        int n =s.size();
+        int op = 0 , cl = 0;
+        stack<int> st;
+        
+        for(int i=0; i<n; i++)
+        {
+            if(s[i]=='(')
+                op++;
+            else if(s[i]==')') 
+                cl++;
+            else
+                st.push(i);
+            
+            while(cl>op && !st.empty()){
+                s[st.top()] = '(';
+                st.pop();
+                op++;
+            }
+            
+            if(cl>op) 
+                return false;
+        }
+        
+        if(op==cl) 
+            return true;
+    
+        int stars = 0;
+        op = 0;
+        cl = 0;
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            if(s[i]=='(')
+                op++;
+            else if(s[i]==')') 
+                cl++;
+            else
+                stars++;
+            
+            while(op>cl && stars>0){
+                cl++;
+                stars--;
+            }
+            if(op>cl) 
+                return false;
+        }
+        return true;
     }
 };
