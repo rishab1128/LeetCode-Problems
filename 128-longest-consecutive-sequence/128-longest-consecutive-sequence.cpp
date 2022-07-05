53631f733b7a09e1fbe4743a30ee438c8055f284
Rishab Purkayastha <63293155+rishab1128@users.noreply.github.com>
2022-07-05 07:09:10 +0530

    Time: 167 ms (73.25%), Space: 52 MB (11.93%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,27 +1,70 @@
+class DSU{
+public:
+    vector<int>rank;
+    vector<int>par;
+    DSU(int n)
+    {
+        rank.resize(n,1);
+        par.resize(n);
+        for(int i=0; i<n; i++)
+            par[i]=i;
+    }
+    void merge(int x , int y) //Using Union By Rank
+    {
+        int xset = find(x);
+        int yset = find(y);
+ 
+        if (xset == yset) 
+            return;
+ 
+        if (rank[xset] < rank[yset])
+        {
+            par[xset] = yset;
+            rank[yset]+=rank[xset];
+        }
+        else
+        {
+            par[yset] = xset;
+            rank[xset]+=rank[yset];
+        }
+    }
+    int find(int x)
+    {
+        if(x==par[x])
+            return x;
+        return par[x] = find(par[x]);
+    }
+};
+
 class Solution {
 public:
-    int longestConsecutive(vector<int>& arr) 
+    int longestConsecutive(vector<int>& nums) 
     {
-        unordered_set<int>st;
-        for(auto x: arr)
-            st.insert(x);
+        int n = nums.size();
         
-        int ans = 0;
-        for(auto ele : st)
+        DSU obj(n);
+        
+        unordered_map<int,int>pos;
+        for(int i=0; i<n; i++)
         {
-            if(!st.count(ele-1))
-            {
-                int currEle = ele;
-                int currLen  = 1;
-                while(st.count(currEle+1)){
-                    currEle++;
-                    currLen++;
-                }
-                ans = max(ans,currLen);
-            }
+            int curr = nums[i] , prev = nums[i]-1 , next = nums[i]+1;
+            
+            if(pos.count(curr))
+                continue;
+            
+            pos[curr] = i;
+            
+            if(pos.count(prev))
+                obj.merge(pos[prev] , pos[curr]);
+            
+            if(pos.count(next))
+                obj.merge(pos[curr] , pos[next]);
         }
         
-        return ans;
+        int ans = 0;
+        for(int i=0; i<n; i++)
+            ans = max(ans, obj.rank[i]);
         
+        return ans;
     }
 };
