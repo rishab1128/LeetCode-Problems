0c61d17ffe7de73e7437c0313cd017c5a76e7767
Rishab Purkayastha <63293155+rishab1128@users.noreply.github.com>
2022-07-08 03:12:47 +0530

    Time: 29 ms (46.90%), Space: 11.8 MB (87.00%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.cpp
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.cpp
@@ -4,19 +4,19 @@ public:
     int minMeetingRooms(vector<vector<int>>& inter) 
     {
         sort(inter.begin(), inter.end());
-        multiset<int>st;
-        st.insert(inter[0][1]);
+        priority_queue<int,vector<int>,greater<>>pq; //stores the minimum end time 
+        pq.push(inter[0][1]);
         int cnt = 1;
         for(int i=1; i<inter.size(); i++)
         {
             int start = inter[i][0] ,  end = inter[i][1];
             
-            if(start>=*st.begin())
-                st.erase(st.begin());
+            if(start>=pq.top())
+                pq.pop();
             else
                 cnt++;
             
-            st.insert(end);
+            pq.push(end);
         }
         
         return cnt;
