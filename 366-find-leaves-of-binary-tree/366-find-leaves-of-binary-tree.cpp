b891b5476139aa47871d841dc1c1503b23b0adfd
Rishab Purkayastha <63293155+rishab1128@users.noreply.github.com>
2022-07-08 02:19:57 +0530

    Time: 0 ms (100.00%), Space: 9.2 MB (7.68%) - LeetHub

--- 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.cpp
+++ 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.cpp
@@ -1,16 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-private:
-    int dfs(TreeNode* root, vector<vector<int>>& res){
-        if(!root) return 0;
-        int level = max(dfs(root->left, res), dfs(root->right, res)) + 1;
-        if(level > (int)res.size()) res.push_back(vector<int>());
-        res[level - 1].push_back(root->val);
-        return level;
-    }
 public:
-    vector<vector<int>> findLeaves(TreeNode* root) {
-        vector<vector<int>> res;
-        dfs(root, res);
-        return res;
+    
+    map<int,vector<int>>mp;
+    
+    int dfs(TreeNode*root)
+    {
+        if(root and !root->left and !root->right)
+        {
+            mp[0].push_back(root->val);
+            return 0;
+        }
+        int leftVal = root->left ? 1+dfs(root->left) : 0;
+        int rightVal = root->right ? 1+dfs(root->right) : 0;
+        
+        mp[max(leftVal,rightVal)].push_back(root->val);
+        
+        return max(leftVal,rightVal);
+    }
+    
+    vector<vector<int>> findLeaves(TreeNode* root) 
+    {
+        dfs(root);
+        vector<vector<int>>ans;
+        for(auto [key,vec]: mp)
+            ans.push_back(vec);
+        
+        return ans;
+        
     }
 };
