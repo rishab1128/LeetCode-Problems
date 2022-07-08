5748090f7a1c2f858ea58e8f6ba7b494713a83c7
Rishab Purkayastha <63293155+rishab1128@users.noreply.github.com>
2022-07-08 15:40:03 +0530

    Time: 34 ms (78.96%), Space: 10.6 MB (56.74%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.cpp
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
-    unordered_map<int,int>dp;
+    vector<int>dp;
     int calc(int i)
     {
         if(i==1)
             return 0;
         
-        if(dp[i])
+        if(i<dp.size() and dp[i]!=-1)
             return dp[i];
         
+        int ans = 0;
         if(i%2)
-            return dp[i] = 1+calc(3*i+1);
+            ans = 1+calc(3*i+1);
+        else
+            ans = 1+calc(i/2);
         
-        return dp[i] = 1+calc(i/2);
+        if(i<dp.size())
+            dp[i] = ans;
+        return ans;
     }
     
     int getKth(int lo, int hi, int k) 
     {
+        dp.resize(1001,-1);
         vector<pair<int,int>>ans;
         for(int i=lo; i<=hi; i++)
             ans.push_back(make_pair(calc(i),i));
