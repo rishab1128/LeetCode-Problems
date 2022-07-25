6e27f061b51bdf52009f108b57e3c182cf563c9d
Rishab Purkayastha <63293155+rishab1128@users.noreply.github.com>
2022-07-25 15:19:45 +0530

    Time: 12 ms (60.23%), Space: 13.7 MB (17.99%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,49 +1,31 @@
 class Solution {
 public:
     
-    int firstPos(vector<int>&arr, int target)
+    int upperBound(vector<int>&arr, int target)
     {
-        int low = 0 , high = arr.size()-1 , pos = -1;
+        int low = 0 , high = arr.size()-1 , pos=arr.size();
         while(low<=high)
         {
-            int mid = (low+high)>>1;
-            if(arr[mid]>=target)
-            {
-                if(arr[mid]==target)
-                    pos = mid;
+            int mid = low+(high-low)/2;
+            if(arr[mid]>target){
+                pos = mid;
                 high = mid-1;
             }
             else
                 low = mid+1;
-        }
-        return pos;
-    }
-    int lastPos(vector<int>&arr, int target)
-    {
-        int low = 0 , high = arr.size()-1 , pos = -1;
-        while(low<=high)
-        {
-            int mid = (low+high)>>1;
-            // cout<<mid<<" "<<arr[mid]<<" "<<pos<<endl;
-            if(arr[mid]<=target)
-            {
-                if(arr[mid]==target)
-                    pos = mid;
-                low = mid+1;
-            }
-            else
-                high = mid-1;
         }
         return pos;
     }
     
     vector<int> searchRange(vector<int>& arr, int target) 
     {
-        int pos1 = firstPos(arr,target);
-        if(pos1==-1)
+        if(arr.size()==0)
+            return {-1,-1};
+        int pos1 = upperBound(arr,target-1);
+        if(pos1==arr.size() || (arr[pos1]!=target))
             return {-1,-1};
         
-        int pos2 = lastPos(arr,target);
+        int pos2 = upperBound(arr,target)-1;
         return {pos1,pos2};
         
     }
