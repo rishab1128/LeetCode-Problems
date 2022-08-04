129088f11c97bd0bcd200c9bf2c0fa051f785241
Rishab Purkayastha <63293155+rishab1128@users.noreply.github.com>
2022-08-04 18:34:42 +0530

    Time: 10 ms (26.53%), Space: 10.9 MB (95.53%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -10,16 +10,16 @@ public:
             if(arr[mid]==target)
                 return mid;
             
-            if(arr[low]<=arr[mid]) //left part is sorted
+            if(arr[low]<=arr[mid]) //left part is sorted ==> [low-mid] is sorted
             {
-                if(arr[low]<=target and target<=arr[mid])
+                if(arr[low]<=target and target<=arr[mid]) //target lies b/w [low-mid]
                     high = mid-1;
                 else
                     low = mid+1;
             }
-            else //right part is sorted
+            else //right part is sorted ==> [mid-high] is sorted
             {
-                if(arr[mid]<=target and target<=arr[high])
+                if(arr[mid]<=target and target<=arr[high]) //target lies b/w [mid-high]
                     low = mid+1;
                 else
                     high = mid-1;
