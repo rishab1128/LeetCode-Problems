0aa90f4885906bfc994c69ed52a643a49e9eec6c
Rishab Purkayastha <63293155+rishab1128@users.noreply.github.com>
2022-08-06 03:22:11 +0530

    Time: 463 ms (66.27%), Space: 117 MB (65.58%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.cpp
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.cpp
@@ -1,42 +1,45 @@
+//maxHeap stores all the low numbers (left part of the median of a sorted array) -> top of maxHeap gives the max among all the low numbers
+
+//minHeap stores all the high numbers (right part of the median of a sorted array) -> top of minHeap gives the min among all the high numbers
+
 class MedianFinder {
-private:
-    priority_queue<int>maxPq;
-    priority_queue<int,vector<int>,greater<>>minPq;
+private: priority_queue<int>maxHeap;
+         priority_queue<int,vector<int>,greater<>>minHeap;
 public:
     MedianFinder() {
         
     }
     
-    void addNum(int num) 
+    //O(log n)
+    void addNum(int x) 
     {
-        if(maxPq.empty() || num<maxPq.top())
-            maxPq.push(num);
+        if(maxHeap.empty() || x<maxHeap.top())
+            maxHeap.push(x);
         else
-            minPq.push(num);
+            minHeap.push(x);
         
-       
-        if((int)maxPq.size()-(int)minPq.size()>1)
+        if((int)maxHeap.size()-(int)minHeap.size()>1)
         {
-            minPq.push(maxPq.top());
-            maxPq.pop();
+            minHeap.push(maxHeap.top());
+            maxHeap.pop();
         }
-        else if((int)minPq.size()-(int)maxPq.size()>1)
+        else if((int)minHeap.size()-(int)maxHeap.size()>1)
         {
-            maxPq.push(minPq.top());
-            minPq.pop();
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
         }
     }
     
-    double findMedian() 
-    {
-        if(maxPq.size()>minPq.size())
-            return (double)maxPq.top();
-        else if(minPq.size()>maxPq.size())
-            return (double)minPq.top();
+    //O(1)
+    double findMedian() {
         
-        double ans = ((double)maxPq.top()+minPq.top())/2;
-        return ans;
+        if((int)maxHeap.size()>(int)minHeap.size())
+            return (double)maxHeap.top();
         
+        if((int)minHeap.size()>(int)maxHeap.size())
+            return (double)minHeap.top();
+        
+        return (double)(maxHeap.top()+minHeap.top())/2.0;
     }
 };
 
