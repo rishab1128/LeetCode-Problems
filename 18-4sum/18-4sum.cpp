0a006fabf453ecdf5df2d9116e443b58fb510e5a
Rishab Purkayastha <63293155+rishab1128@users.noreply.github.com>
2022-08-09 21:00:01 +0530

    Time: 145 ms (37.68%), Space: 13 MB (92.42%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -1,40 +1,51 @@
+using ll = long long;
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) 
+    vector<vector<int>> fourSum(vector<int>& arr, int target) 
     {
-        using ll = long long;
-        int n=nums.size() ;
-        set<vector<int>>st;
-        map<long long,int>mp;
-        sort(nums.begin() , nums.end() );
-        for(int i=0; i<n; i++)
-            mp[nums[i]]=i;
-        // for(auto x: nums)
-        //     cout<<x<<" ";
-        // cout<<endl;
-        
-        for(int i=0; i<n; i++)
-        {
-            //cout<<nums[i]<<endl;
-            for(int j=i+1; j<n; j++)
-            {
-                for(int k=j+1; k<n; k++)
-                {
-                    long long tot = (ll)nums[i]+(ll)nums[j]+(ll)nums[k];
-                    long long search=target-tot;
-                    //cout<<nums[i]<<" "<<search<<endl;
-                    if(mp.find(search)!=mp.end() and mp[search]>k)
-                    {
-                        st.insert({nums[i],nums[j],nums[k],(int)search});
-                    }
-                        
-                }
-            }
-        }
-        
         vector<vector<int>>ans;
-        for(auto x: st)
-            ans.push_back(x);
+        sort(arr.begin() , arr.end());
+        int n = arr.size();
+        for(int i=0; i<n-3; i++)
+        {
+            for(int j=i+1; j<n-2; j++)
+            {
+                
+                ll sum = (ll)target - (ll)(arr[i]+arr[j]);
+                
+                int left = j+1 , right = n-1;
+                
+                while(left<right)
+                {
+                    if(arr[left]+arr[right]==sum)
+                    {
+                        ans.push_back({arr[i],arr[j],arr[left],arr[right]});
+                        
+                        while(left+1<n and arr[left]==arr[left+1])
+                            left++;
+                        while(right-1>=0 and arr[right]==arr[right-1])
+                            right--;
+                        
+                        left++;
+                        right--;
+                    }
+                    else if(arr[left]+arr[right]<sum)
+                        left++;
+                    else
+                        right--;
+                }
+                
+                int k = j;
+                while(k+1<n and arr[k]==arr[k+1])
+                    k++;
+                j = k;
+            }
+            
+            int k = i;
+            while(k+1<n and arr[k]==arr[k+1])
+                k++;
+            i = k;
+        }
         
         return ans;
     }
