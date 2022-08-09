639bb3f2cce9438557bdea52c5b142e2e6a622c6
Rishab Purkayastha <63293155+rishab1128@users.noreply.github.com>
2022-08-09 20:32:54 +0530

    Time: 1325 ms (7.04%), Space: 49.8 MB (36.55%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,70 +1,30 @@
-class DSU{
-public:
-    vector<int>rank;
-    vector<int>par;
-    DSU(int n)
-    {
-        rank.resize(n,1);
-        par.resize(n);
-        for(int i=0; i<n; i++)
-            par[i]=i;
-    }
-    void merge(int x , int y) //Using Union By Rank
-    {
-        int xset = find(x);
-        int yset = find(y);
- 
-        if (xset == yset) 
-            return;
- 
-        if (rank[xset] < rank[yset])
-        {
-            par[xset] = yset;
-            rank[yset]+=rank[xset];
-        }
-        else
-        {
-            par[yset] = xset;
-            rank[xset]+=rank[yset];
-        }
-    }
-    int find(int x)
-    {
-        if(x==par[x])
-            return x;
-        return par[x] = find(par[x]);
-    }
-};
-
 class Solution {
 public:
-    int longestConsecutive(vector<int>& nums) 
+    int longestConsecutive(vector<int>& arr) 
     {
-        int n = nums.size();
+        int n = arr.size();
         
-        DSU obj(n);
-        
-        unordered_map<int,int>pos;
-        for(int i=0; i<n; i++)
-        {
-            int curr = nums[i] , prev = nums[i]-1 , next = nums[i]+1;
-            
-            if(pos.count(curr))
-                continue;
-            
-            pos[curr] = i;
-            
-            if(pos.count(prev))
-                obj.merge(pos[prev] , pos[curr]);
-            
-            if(pos.count(next))
-                obj.merge(pos[curr] , pos[next]);
-        }
+        unordered_set<int>st;
+        for(auto x: arr)
+            st.insert(x);
         
         int ans = 0;
         for(int i=0; i<n; i++)
-            ans = max(ans, obj.rank[i]);
+        {
+            if(!st.count(arr[i]-1))
+            {
+                int currEle = arr[i];
+                int currLen = 1;
+                while(st.count(currEle+1))
+                {
+                    currEle++;
+                    currLen++;
+                }
+                ans = max(ans,currLen);
+            }
+        }
         
         return ans;
+        
     }
 };
